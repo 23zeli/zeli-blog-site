19bb789714b7e44a91a203399e1f699239f80052
Mtutuzeli <mtutuzeli@gmail.com>
2026-07-31 18:25:13 +0200

    remove commented-out code for post list in PostPage component

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -23,21 +23,6 @@ export default async function PostPage({ params }: PostPageProps) {
             </h1>
 
             <p className="text-lg leading text-zinc-700">{post.body}</p>
-
-            {/* <ul className="space--y-3">
-                {posts.slice(0, 5).map((post) => (
-                    <li key={post.id}>
-                        <div className="flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
-                            <Link
-                                href={`posts/${post.id}`}
-                                className="text-lg font-semibold text-zinc-950 hove:text-zinc-200"
-                            >
-                                {post.title.charAt(0).toUpperCase() + post.title.slice(1)}
-                            </Link>
-                        </div>
-                    </li>
-                ))}
-            </ul> */}
         </article>
     </div>
   )
