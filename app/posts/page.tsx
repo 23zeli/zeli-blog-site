d9715ac44be8d93335cfc4a6a6f533c206bd6d65
Mtutuzeli <mtutuzeli@gmail.com>
2026-07-31 17:41:36 +0200

    ``` No changes made ```

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,15 +1,20 @@
 import Link from "next/link";
 
-type Posts = {
+type Post = {
     userId: number;
     id: number;
     title: string;
     body: string;
 };
 
+
+
 export default async function PostPage() {
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const posts = await response.json();
+    if(!response.ok) {
+        throw new Error("failed to fetch Posts")
+    }
+    const posts: Post [] = await response.json();
 
   return (
     <div className="space-y-8">
@@ -18,13 +23,13 @@ export default async function PostPage() {
                 Posts
             </h1>
 
-            <ul className="space--y-3">
+            <ul className="space-y-3">
                 {posts.slice(0, 5).map((post) => (
                     <li key={post.id}>
                         <div className="flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
                             <Link
-                                href={`posts/${post.id}`}
-                                className="text-lg font-semibold text-zinc-950 hove:text-zinc-200"
+                                href={`/posts/${post.id}`}
+                                className="text-lg font-semibold text-zinc-950 hover:text-zinc-200"
                             >
                                 {post.title.charAt(0).toUpperCase() + post.title.slice(1)}
                             </Link>
