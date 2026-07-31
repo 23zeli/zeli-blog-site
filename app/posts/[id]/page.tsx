eb1ebfb9083d592a5067f272a18417fc41b46b9f
Mtutuzeli <mtutuzeli@gmail.com>
2026-07-31 17:06:09 +0200

    removed 's' from posts

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,6 +1,6 @@
 export default async function PostPage() {
     const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
-    const posts = await response.json();
+    const post = await response.json();
 
   return (
     <div className="space-y-8">
