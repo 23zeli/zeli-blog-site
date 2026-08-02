6c3dd4474ee4c5f6e86fb4fa70cb8ce54e73e032
Mtutuzeli <mtutuzeli@gmail.com>
2026-08-02 09:51:57 +0200

    refactor PostPage component to use PostList for fetching and displaying post data with loading state

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,23 +1,16 @@
 import { notFound } from "next/navigation";
+import { Suspense } from "react";
 // import { prisma } from "../lib/prisma";
 
 type PostPageProps = {
     params: Promise<{id: string}>
 }
 
-
-export default async function PostPage({ params }: PostPageProps) {
+async function PostList ({ params }: PostPageProps) {
     const { id } = await params;
 
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
     const post = await response.json();
-
-    // const post = await prisma.post.findUnique({
-    //     where: {
-    //         id: Number(id),
-    //     },
-    // });
-
 
     //check if post exists
     if(!post) {
@@ -25,15 +18,32 @@ export default async function PostPage({ params }: PostPageProps) {
     }
 
   return (
-    <div className="space-y-8">
-        <article className="space-y-4">
-            <h1 className="text-center text-4xl font-semibold text-zinc-950 sm:text-5xl">
-                {post.title.charAt(0).toUpperCase() + post.title.slice(1)}
-            </h1>
+    <article className="space-y-4">
+        <h1 className="text-center text-4xl font-semibold text-zinc-950 sm:text-5xl">
+            {post.title.charAt(0).toUpperCase() + post.title.slice(1)}
+        </h1>
 
-            <p className="text-lg leading text-zinc-700">{post.body}</p>
-            {/* <p className="text-lg leading text-zinc-700">{post.content}</p> */}
-        </article>
+        <p className="text-lg leading text-zinc-700">{post.body}</p>
+        {/* <p className="text-lg leading text-zinc-700">{post.content}</p> */}
+    </article>
+  )
+}
+
+
+
+export default async function PostPage({ params }: PostPageProps) {
+
+    // const post = await prisma.post.findUnique({
+    //     where: {
+    //         id: Number(id),
+    //     },
+    // });
+
+  return (
+    <div className="space-y-8">
+        <Suspense fallback={<p>Loading post...</p>}>
+            <PostList params={params} />
+        </Suspense>
     </div>
   )
 }
