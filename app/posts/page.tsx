791429c4357a90546d1969c5cf2c6604b0fc7bcf
Mtutuzeli <mtutuzeli@gmail.com>
2026-08-07 15:23:57 +0200

    refactor PostPage component to utilize getPosts function for fetching post data

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,75 +1,18 @@
-// import RecentlyViewedPosts from "@/components/recently-viewed-posts";
-// import Link from "next/link";
-// import { Suspense } from "react";
-// import { prisma } from "./lib/prisma";
-
-// type Post = {
-//     userId: number;
-//     id: number;
-//     title: string;
-//     body: string;
-// };
-
-
-
-// export default async function PostPage() {
-
-//     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-//     if(!response.ok) {
-//         throw new Error("failed to fetch Posts")
-//     }
-//     const posts: Post [] = await response.json();
-
-//     const posts = await prisma.post.findMany();
-
-//   return (
-//     <div className="space-y-8">
-//         <section className="space-y-4">
-//             <h1 className="text-center text-4xl font-semibold text-zinc-950 sm:text-5xl">
-//                 Posts
-//             </h1>
-
-//             <ul className="space-y-3">
-//                 {posts.slice(0, 5).map((post) => (
-//                     <li key={post.id}>
-//                         <div className="flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
-//                             <Link
-//                                 href={`/posts/${post.id}`}
-//                                 className="text-lg font-semibold text-zinc-950 hover:text-zinc-200"
-//                             >
-//                                 {post.title.charAt(0).toUpperCase() + post.title.slice(1)}
-//                             </Link>
-//                         </div>
-//                     </li>
-//                 ))}
-//             </ul>
-
-//             <Suspense fallback={<p>Loading recently viewed posts...</p>}>
-//                 <RecentlyViewedPosts />
-//             </Suspense>
-//         </section>
-//     </div>
-//   )
-// }
-
 import RecentlyViewedPosts from "@/components/recently-viewed-posts";
 import Link from "next/link";
 import { Suspense } from "react";
+import { getPosts } from "./lib/utils";
 // import { prisma } from "./lib/prisma";
 
-type Post = {
-    userId: number;
-    id: number;
-    title: string;
-    body: string;
-};
 
 async function PostsList() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-    if (!response.ok) {
-        throw new Error("failed to fetch Posts");
-    }
-    const posts: Post[] = await response.json();
+
+    const posts =  await getPosts();
+    // const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    // if (!response.ok) {
+    //     throw new Error("failed to fetch Posts");
+    // }
+    // const posts: Post[] = await response.json();
 
     // const posts = await prisma.post.findMany();
 
