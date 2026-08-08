f6af6cc541975883a8095ad51be9201414079bc9
Mtutuzeli <mtutuzeli@gmail.com>
2026-08-08 14:33:24 +0200

    add createPost function for handling post creation in actions

---
+++ actions/actions.ts
@@ -0,0 +1,16 @@
+"use server";
+
+import { prisma } from "@/app/posts/lib/prisma";
+
+async function createPost(formData: FormData) {
+
+    const title = String(formData.get("title") ?? "").trim();
+    const content = String(formData.get("content") ?? "").trim();
+
+    await prisma.post.create({
+        data: {
+            title,
+            content,
+        },
+    });
+}
