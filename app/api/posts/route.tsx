582b13e576d40669c7b553fa58367c604f2bfa5e
Mtutuzeli <mtutuzeli@gmail.com>
2026-08-16 09:23:11 +0200

    feat: enhance POST endpoint to handle request body and return created post

--- app/api/posts/route.tsx
+++ app/api/posts/route.tsx
@@ -1,14 +1,17 @@
 import { prisma } from "@/app/posts/lib/prisma";
 
 export async function GET() {
+
     const posts = await prisma.post.findMany();
 
     return Response.json({ posts });
 }
 
 export async function POST(request: Request) {
+    //grab data(title, content) from the request(post being created) body
     const { title, content } = await request.json();
 
+    //prisma ORM method to create a new post in the database
     const post = await prisma.post.create({
         data: {
             title,
@@ -16,5 +19,6 @@ export async function POST(request: Request) {
         },
     })
 
+    //return the newly created post as a JSON response
     return Response.json({ post });
 }
