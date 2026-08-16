3b793a413e9ee42d4009d0074bcddb1268c9c300
Mtutuzeli <mtutuzeli@gmail.com>
2026-08-16 10:15:21 +0200

    style: update UpvoteBtn button styles for consistency and improved appearance

--- components/upp-vote.tsx
+++ components/upp-vote.tsx
@@ -14,8 +14,8 @@ export default function UpvoteBtn({ postId,votes}: UpvoteBtnProps) {
     <button
         type='button'
         onClick={()=> upvotePost(postId)}
-        className='bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors duration-300'
-        // className='h-9 shrink-0 rounded-md border border-zinc-300 bg-zinc-100 px-3 text-sm font-semibold text-zinc-800 transition-colors hover:bg-zinc-200'
+        // className='bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors duration-300'
+        className='h-9 shrink-0 rounded-md border border-zinc-300 bg-zinc-100 px-3 text-sm font-semibold text-zinc-800 transition-colors hover:bg-zinc-200'
     >
         Upvote {votes}
     </button>
