3fa5f0f15c28681140375d94c050f1cec5e7e31e
Mtutuzeli <mtutuzeli@gmail.com>
2026-08-21 14:28:36 +0200

    feat: add server-side authentication check in createPost action

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,11 +1,19 @@
 "use server";
 
 import { prisma } from "@/app/posts/lib/prisma";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import {
     // revalidatePath,
     revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export async function createPost(formData: FormData) {
+
+    const { isAuthenticated } = getKindeServerSession();
+    if (!(await isAuthenticated())) {
+        // redirect("/api/auth/kindeAuth");
+        redirect("/api/auth/login");
+    }
 
     const title = String(formData.get("title") ?? "").trim();
     const content = String(formData.get("content") ?? "").trim();
