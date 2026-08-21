1e885f3fc786b05e10ba49ab33e8c30eae26f00a
Mtutuzeli <mtutuzeli@gmail.com>
2026-08-21 13:41:07 +0200

    docs: add comments to clarify proxy function use cases

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,14 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
+//Some common scenarios where Proxy is effective include:
+
+//Modifying headers for all pages or a subset of pages
+//Rewriting to different pages based on A/B test or experiments
+//Programmatic redirects based on incoming request properties
+
+// authentication?
+
 export default function proxy(request: NextRequest) {
     // Redirect requests to /admin to the home page
     if (request.nextUrl.pathname.startsWith('/admin')) {
