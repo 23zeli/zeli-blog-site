c0dcc36fe5b1fbc7beb15f38519d1cd9311c833d
Mtutuzeli <mtutuzeli@gmail.com>
2026-08-21 12:38:28 +0200

    feat: redirect requests from /admin to the home page in proxy function

--- proxy.ts
+++ proxy.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export default function proxy(request: NextRequest) {
+    // Redirect requests to /admin to the home page
     if (request.nextUrl.pathname.startsWith('/admin')) {
         return NextResponse.redirect(new URL('/', request.url));
     }
